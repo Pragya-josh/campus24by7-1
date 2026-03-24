f223f86fec74b9a8afb551b13940c0761b0d68f1
aivora-technologies <connect.aivoratechnologies@gmail.com>
2026-03-24 15:08:20 +0530

    removed out folder from git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,7 +135,7 @@ export default function RootLayout({
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
-                        __html: JSON.stringify(structuredData)
+                        __html: JSON.stringify({ "@graph": structuredData })
                     }}
                 />
             </body>
